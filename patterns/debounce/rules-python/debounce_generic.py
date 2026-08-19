e14c4f645fb3d78390fb679a1c1698c8be6653f4
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-19 23:09:02 +0200

    Refactor PresenceDetectionDebounce execution method
    Refactor debounce logic to use event input and improve state comparison.

--- patterns/debounce/rules-python/debounce_generic.py
+++ patterns/debounce/rules-python/debounce_generic.py
@@ -12,21 +12,27 @@ timers = {}
 ])
 class PresenceDetectionDebounce:
     def execute(self, module, input):
-        item_name = input["itemName"]
+        event = input.get("event")
+        if not event:
+            return
+
+        item_name = event.getItemName()
         triggering_item = Registry.getItem(item_name)
 
         if item_name in timers:
             timers[item_name].cancel()
 
-        delay = 0 if str(triggering_item.state) == "ON" else 2 * 60
+        delay = 0 if str(triggering_item.getState()) == "ON" else 2 * 60
 
         def on_expire(name=item_name, item=triggering_item):
             proxy_name = name.split("_")[0] + "_Present"
             proxy_item = Registry.getItem(proxy_name)
-            if str(proxy_item.state) != str(item.state):
-                proxy_item.sendCommand(item.state)
+            current_state = str(item.getState())
+            if str(proxy_item.getState()) != current_state:
+                proxy_item.sendCommand(current_state)
             del timers[name]
 
         t = threading.Timer(delay, on_expire)
         t.start()
         timers[item_name] = t
+        self.logger.info(item_name + ": Debounce-Timer gestartet, Verzoegerung=" + str(delay) + "s")
