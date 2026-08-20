5222d89c3e3871dfdc4283c4cfc0ced95aebac7f
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 17:39:35 +0200

    Refactor state retrieval methods in SetLightsBasedOnTimeOfDay

--- patterns/state-machine-driven-groups/rules-python/state_machine_driven_groups_complex.py
+++ patterns/state-machine-driven-groups/rules-python/state_machine_driven_groups_complex.py
@@ -5,13 +5,14 @@ from openhab.triggers import ItemStateChangeTrigger
 @rule(triggers=[ItemStateChangeTrigger("vTimeOfDay")])
 class SetLightsBasedOnTimeOfDay:
     def execute(self, module, input):
-        time_of_day = str(Registry.getItem("vTimeOfDay").state)
+        time_of_day = str(Registry.getItem("vTimeOfDay").getState())
 
-        for light in Registry.getItem("gLights").members:
+        for light in Registry.getItem("gLights").getAllMembers():
             setting = next(
-                (s for s in Registry.getItem("gSettings").members
-                 if s.name == light.name + "_" + time_of_day),
+                (s for s in Registry.getItem("gSettings").getAllMembers()
+                 if s.getName() == light.getName() + "_" + time_of_day),
                 None,
             )
             if setting is not None:
-                light.sendCommand(setting.state)
+                light.sendCommand(setting.getState())
+                self.logger.info(light.getName() + " -> " + str(setting.getState()))
