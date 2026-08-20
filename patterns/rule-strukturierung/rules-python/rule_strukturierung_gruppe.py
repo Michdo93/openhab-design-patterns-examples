119e7b1e36ac5e813525f614be6b22f1c5276a63
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 17:12:42 +0200

    Refactor GroupApproach to use getAllMembers

--- patterns/rule-strukturierung/rules-python/rule_strukturierung_gruppe.py
+++ patterns/rule-strukturierung/rules-python/rule_strukturierung_gruppe.py
@@ -5,9 +5,11 @@ from openhab.triggers import GroupStateChangeTrigger
 @rule(triggers=[GroupStateChangeTrigger("MyGroup")])
 class GroupApproach:
     def execute(self, module, input):
-        members = Registry.getItem("MyGroup").members
-        if any(str(i.state) == "NULL" for i in members):
+        members = Registry.getItem("MyGroup").getAllMembers()
+        if any(str(i.getState()) == "NULL" for i in members):
             self.logger.warn("One of the Items is NULL")
             return
-        on_count = sum(1 for i in members if str(i.state) == "ON")
-        Registry.getItem("Buzz").sendCommand("ON" if on_count >= 2 else "OFF")
+        on_count = sum(1 for i in members if str(i.getState()) == "ON")
+        new_state = "ON" if on_count >= 2 else "OFF"
+        Registry.getItem("Buzz").sendCommand(new_state)
+        self.logger.info("on_count={} -> Buzz={}".format(on_count, new_state))
