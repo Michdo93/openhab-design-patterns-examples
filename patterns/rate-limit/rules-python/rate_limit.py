0036956fac8117814cfdac53bc3178c33831e30d
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 16:26:05 +0200

    Implement rate limiting with OpenHAB rule
    Refactor rate limiting logic into a class with a trigger.

--- patterns/rate-limit/rules-python/rate_limit.py
+++ patterns/rate-limit/rules-python/rate_limit.py
@@ -1,10 +1,18 @@
 from datetime import datetime, timedelta
 
+from openhab import rule
+from openhab.triggers import ItemCommandTrigger
 from scope import cache
 
-last_action = cache.privateCache.get("LastAction")
-now = datetime.now().astimezone()
 
-if last_action is None or last_action < now - timedelta(hours=24):
-    print("Rate-limited action")
-    cache.privateCache.put("LastAction", now)
+@rule(triggers=[ItemCommandTrigger("RateLimitTrigger", "ON")])
+class RateLimitBeispiel:
+    def execute(self, module, input):
+        last_action = cache.privateCache.get("LastAction")
+        now = datetime.now().astimezone()
+
+        if last_action is None or last_action < now - timedelta(hours=24):
+            self.logger.info("Rate-limited action")
+            cache.privateCache.put("LastAction", now)
+        else:
+            self.logger.info("Ereignis ignoriert, Sperrzeit laeuft noch")
