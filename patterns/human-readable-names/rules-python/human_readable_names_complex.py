77cd74986480c60e86c46edef643d0c0ef9e3aa4
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 13:13:10 +0200

    Refactor StatusAlert to use event for state handling

--- patterns/human-readable-names/rules-python/human_readable_names_complex.py
+++ patterns/human-readable-names/rules-python/human_readable_names_complex.py
@@ -10,19 +10,23 @@ alert_timers = {}
 @rule(triggers=[GroupStateChangeTrigger("gSensorStatus")])
 class StatusAlert:
     def execute(self, module, input):
-        old_state = input.get("oldItemState")
-        if old_state is None:
+        event = input.get("event")
+        if not event:
             return
 
-        item_name = input["itemName"]
+        old_state = event.getOldItemState()
+        if old_state is None or str(old_state) in ("NULL", "UNDEF"):
+            return
+
+        item_name = event.getItemName()
         if item_name in alert_timers:
             alert_timers[item_name].cancel()
 
-        orig_state = str(Registry.getItem(item_name).state)
+        orig_state = str(Registry.getItem(item_name).getState())
 
         def on_expire():
             del alert_timers[item_name]
-            if str(Registry.getItem(item_name).state) == orig_state:
+            if str(Registry.getItem(item_name).getState()) == orig_state:
                 name = Transformation.transform("MAP", "admin.map", item_name) or item_name
                 state_name = Transformation.transform("MAP", "admin.map", orig_state) or orig_state
 
