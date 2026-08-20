e2ea1ef54415fcfc501fc0212e96770bd475d9a7
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 17:23:40 +0200

    Refactor sensor aggregation logic for state retrieval

--- patterns/sensor-aggregation/rules-python/sensor_aggregation.py
+++ patterns/sensor-aggregation/rules-python/sensor_aggregation.py
@@ -10,33 +10,33 @@ from openhab.triggers import (
 class ResetVPresentToOffOnStartup:
     def execute(self, module, input):
         Registry.getItem("vPresent").sendCommand("OFF")
-        for s in Registry.getItem("gPresent").members:
+        for s in Registry.getItem("gPresent").getAllMembers():
             s.sendCommand("OFF")
 
 
 @rule(triggers=[GroupStateChangeTrigger("gPresent")])
 class APresenceSensorUpdated:
     def execute(self, module, input):
-        count = float(str(Registry.getItem("gPresent").state))
+        count = float(str(Registry.getItem("gPresent").getState()))
         v_present = Registry.getItem("vPresent")
         t_present = Registry.getItem("tPresent")
 
-        self.logger.debug("gPresent changed to " + str(count))
-        if count > 0 and (str(v_present.state) != "ON" or str(t_present.state) == "ON"):
-            self.logger.debug("Someone came home")
-            if str(t_present.state) != "OFF":
+        self.logger.info("gPresent changed to " + str(count))
+        if count > 0 and (str(v_present.getState()) != "ON" or str(t_present.getState()) == "ON"):
+            self.logger.info("Someone came home")
+            if str(t_present.getState()) != "OFF":
                 t_present.postUpdate("OFF")
-            if str(v_present.state) != "ON":
+            if str(v_present.getState()) != "ON":
                 v_present.sendCommand("ON")
-        elif count == 0 and str(v_present.state) != "OFF" and str(t_present.state) != "ON":
-            self.logger.debug("Everyone is away, setting timer")
+        elif count == 0 and str(v_present.getState()) != "OFF" and str(t_present.getState()) != "ON":
+            self.logger.info("Everyone is away, setting timer")
             t_present.sendCommand("ON")
 
 
 @rule(triggers=[ItemCommandTrigger("tPresent", "OFF")])
 class PresentTimerExpiredNoOneIsHome:
     def execute(self, module, input):
-        count = float(str(Registry.getItem("gPresent").state))
+        count = float(str(Registry.getItem("gPresent").getState()))
         if count == 0:
             self.logger.info("Everyone is away, setting house to away")
             Registry.getItem("vPresent").sendCommand("OFF")
