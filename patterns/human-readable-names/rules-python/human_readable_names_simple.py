2b1960734d0c6e75a62121c10075c68717ce59c7
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 13:12:56 +0200

    Add rule for human-readable name transformation

--- patterns/human-readable-names/rules-python/human_readable_names_simple.py
+++ patterns/human-readable-names/rules-python/human_readable_names_simple.py
@@ -1,3 +1,10 @@
+from openhab import rule, Registry
 from openhab.actions import Transformation
+from openhab.triggers import ItemStateChangeTrigger
 
-name = Transformation.transform("MAP", "admin.map", "MyItem") or "MyItem"
+
+@rule(triggers=[ItemStateChangeTrigger("MyItem")])
+class MenschenlesbarerNameEinfach:
+    def execute(self, module, input):
+        name = Transformation.transform("MAP", "admin.map", "MyItem") or "MyItem"
+        self.logger.info(name + " ist jetzt " + str(Registry.getItem("MyItem").getState()))
