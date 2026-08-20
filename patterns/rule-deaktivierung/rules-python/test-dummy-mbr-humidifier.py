c59345e346b00121d120bf3d54e03c9bde1f3bf1
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 16:43:33 +0200

    Update test-dummy-mbr-humidifier.py

--- patterns/rule-deaktivierung/rules-python/test-dummy-mbr-humidifier.py
+++ patterns/rule-deaktivierung/rules-python/test-dummy-mbr-humidifier.py
@@ -1,8 +1,11 @@
 # Reine Test-Hilfsdatei, um "rule_deaktivierung.py" testen zu koennen -
 # im echten Einsatz waere das eure tatsaechliche Luftbefeuchter-Regel.
 from openhab import rule
+from openhab.triggers import ItemCommandTrigger
 
-@rule(uid="mbr_humidifier", name="MBR Humidifier (Test-Dummy)", triggers=[])
+
+@rule(uid="mbr_humidifier", name="MBR Humidifier (Test-Dummy)",
+      triggers=[ItemCommandTrigger("DummyRuleTrigger")])
 class MbrHumidifierDummy:
     def execute(self, module, input):
         pass
