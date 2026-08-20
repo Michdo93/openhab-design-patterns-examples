dade5a9903e93892670836a3b77f07e3caccbc03
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 16:15:59 +0200

    Refactor command and state retrieval in proxy_item.py

--- patterns/proxy-item/rules-python/proxy_item.py
+++ patterns/proxy-item/rules-python/proxy_item.py
@@ -5,16 +5,26 @@ from openhab.triggers import ItemCommandTrigger, ItemStateUpdateTrigger
 @rule(triggers=[ItemCommandTrigger("ProxySwitch")])
 class ProxySwitchErhieltBefehl:
     def execute(self, module, input):
-        command = str(input["command"])
+        event = input.get("event")
+        if not event:
+            return
+
+        command = str(event.getItemCommand())
         bound = Registry.getItem("BoundSwitch")
-        if str(bound.state) != command:
+        if str(bound.getState()) != command:
             bound.sendCommand(command)
+            self.logger.info("ProxySwitch -> BoundSwitch: " + command)
 
 
 @rule(triggers=[ItemStateUpdateTrigger("BoundSwitchUpdates")])
 class BoundSwitchUpdatesErhieltUpdate:
     def execute(self, module, input):
-        state = str(input["state"])
+        event = input.get("event")
+        if not event:
+            return
+
+        state = str(event.getItemState())
         proxy = Registry.getItem("ProxySwitch")
-        if str(proxy.state) != state:
+        if str(proxy.getState()) != state:
             proxy.postUpdate(state)
+            self.logger.info("BoundSwitchUpdates -> ProxySwitch: " + state)
