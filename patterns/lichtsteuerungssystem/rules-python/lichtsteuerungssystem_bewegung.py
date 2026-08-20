aaf2afdab9665f9ef729d23b6d7ce41029c585cc
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 14:43:27 +0200

    Fix state check for Light1 in motion detection

--- patterns/lichtsteuerungssystem/rules-python/lichtsteuerungssystem_bewegung.py
+++ patterns/lichtsteuerungssystem/rules-python/lichtsteuerungssystem_bewegung.py
@@ -7,10 +7,13 @@ from openhab.triggers import ItemStateUpdateTrigger
 @rule(triggers=[ItemStateUpdateTrigger("MotionSensor1", "ON")])
 class MotionDetected:
     def execute(self, module, input):
-        if str(Registry.getItem("Light1").state) == "OFF":
+        if str(Registry.getItem("Light1").getState()) == "OFF":
             Registry.getItem("Light1").sendCommand("ON")
+            self.logger.info("Bewegung erkannt - Light1 eingeschaltet (10 Min. Timer)")
 
             def turn_off():
                 Registry.getItem("Light1").sendCommand("OFF")
 
             threading.Timer(10 * 60, turn_off).start()
+        else:
+            self.logger.info("Bewegung erkannt, Light1 war aber bereits an")
