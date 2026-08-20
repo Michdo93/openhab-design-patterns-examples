7436284e3f666cbcde42ff342f7668e2c85ca594
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 16:54:49 +0200

    Fix state retrieval for exampleRule in rule manager
    Updated method to retrieve state of exampleRule for enabling/disabling.

--- patterns/rule-manager/rules-python/rule_manager_toggle.py
+++ patterns/rule-manager/rules-python/rule_manager_toggle.py
@@ -8,5 +8,6 @@ rule_manager = getService("org.openhab.core.automation.RuleManager")
 @rule(triggers=[ItemStateChangeTrigger("exampleRule")])
 class EnableDisableExampleRule:
     def execute(self, module, input):
-        enabled = str(Registry.getItem("exampleRule").state) == "ON"
+        enabled = str(Registry.getItem("exampleRule").getState()) == "ON"
         rule_manager.setEnabled("example_rule_uid", enabled)
+        self.logger.info("example_rule_uid enabled=" + str(enabled))
