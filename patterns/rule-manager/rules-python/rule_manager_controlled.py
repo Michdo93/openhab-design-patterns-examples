216ebdb9ad28c42b0c729fca546fae62c735a937
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 16:55:05 +0200

    Update ExampleRule to log execution steps

--- patterns/rule-manager/rules-python/rule_manager_controlled.py
+++ patterns/rule-manager/rules-python/rule_manager_controlled.py
@@ -1,19 +1,21 @@
 from openhab import rule, Registry
+from openhab.triggers import ItemCommandTrigger
 
 
-@rule(triggers=[])  # Trigger der Beispielregel
+@rule(uid="example_rule_uid", name="ExampleRule (kontrollierte Ausfuehrung)",
+      triggers=[ItemCommandTrigger("DummyExecTrigger")])
 class ExampleRule:
     def execute(self, module, input):
         Registry.getItem("isRunningExampleRule").sendCommand("ON")
 
-        if str(Registry.getItem("isRunningExampleRule").state) == "ON":
-            pass  # Teil 1 der Regel
+        if str(Registry.getItem("isRunningExampleRule").getState()) == "ON":
+            self.logger.info("Teil 1 der Regel wird ausgefuehrt")
         else:
-            pass  # Aenderungen rueckgaengig machen
+            self.logger.info("Teil 1: Aenderungen rueckgaengig gemacht (Abbruch erkannt)")
 
-        if str(Registry.getItem("isRunningExampleRule").state) == "ON":
-            pass  # Teil 2 der Regel
+        if str(Registry.getItem("isRunningExampleRule").getState()) == "ON":
+            self.logger.info("Teil 2 der Regel wird ausgefuehrt")
         else:
-            pass  # Aenderungen rueckgaengig machen
+            self.logger.info("Teil 2: Aenderungen rueckgaengig gemacht (Abbruch erkannt)")
 
         Registry.getItem("isRunningExampleRule").sendCommand("OFF")
