8ef89ca8cf07f153aee264942bd363446588e9c8
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 15:02:30 +0200

    Fix method calls to get state of items

--- patterns/looping-timers/rules-python/looping_timers_expire.py
+++ patterns/looping-timers/rules-python/looping_timers_expire.py
@@ -5,25 +5,25 @@ from openhab.triggers import ItemStateChangeTrigger, ItemCommandTrigger
 @rule(triggers=[ItemStateChangeTrigger("MotionSensor", state="ON")])
 class CeilingFanControl:
     def execute(self, module, input):
-        if str(Registry.getItem("CeilingFanTimer").state) != "ON":
+        if str(Registry.getItem("CeilingFanTimer").getState()) != "ON":
             Registry.getItem("CeilingFanTimer").sendCommand("OFF")  # Timer starten
 
 
 @rule(triggers=[ItemCommandTrigger("CeilingFanTimer", "OFF")])
 class CeilingFanLoop:
     def execute(self, module, input):
-        if str(Registry.getItem("vTimeOfDay").state) != "NIGHT":
+        if str(Registry.getItem("vTimeOfDay").getState()) != "NIGHT":
             return
 
         new_state = "STAY"
-        current = float(str(Registry.getItem("CurrentTemp").state))
-        target = float(str(Registry.getItem("TargetTemp").state))
+        current = float(str(Registry.getItem("CurrentTemp").getState()))
+        target = float(str(Registry.getItem("TargetTemp").getState()))
         if current > target:
             new_state = "ON"
         elif current < target - 1:
             new_state = "OFF"
 
-        if new_state != "STAY" and str(Registry.getItem("Fan").state) != new_state:
+        if new_state != "STAY" and str(Registry.getItem("Fan").getState()) != new_state:
             Registry.getItem("Fan").sendCommand(new_state)
 
         Registry.getItem("CeilingFanTimer").sendCommand("ON")
