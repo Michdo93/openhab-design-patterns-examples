6e46694d262902809dac86b791a10517ada5ddf3
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 15:23:12 +0200

    Refactor LightControlReceivedCommand execution logic
    Refactor command handling to use event methods for state checks.

--- patterns/manuelle-trigger-erkennung/rules-python/manuelle_trigger_erkennung_proxy.py
+++ patterns/manuelle-trigger-erkennung/rules-python/manuelle_trigger_erkennung_proxy.py
@@ -5,8 +5,12 @@ from openhab.triggers import GroupCommandTrigger
 @rule(triggers=[GroupCommandTrigger("LightControls")])
 class LightControlReceivedCommand:
     def execute(self, module, input):
-        item_name = input["itemName"]
-        command = input["command"]
+        event = input.get("event")
+        if not event:
+            return
+
+        item_name = event.getItemName()
+        command = str(event.getItemCommand())
         light_name, source = item_name.split("_")[0], item_name.split("_")[1]
 
         if source == "Proxy":
@@ -17,11 +21,13 @@ class LightControlReceivedCommand:
         ui = Registry.getItem(light_name + "_UI")
         rules_item = Registry.getItem(light_name + "_Rules")
 
-        if str(proxy.state) != str(command):
+        if str(proxy.getState()) != command:
             proxy.postUpdate(command)
-        if str(ui.state) != str(command):
+        if str(ui.getState()) != command:
             ui.postUpdate(command)
-        if str(rules_item.state) != str(command):
+        if str(rules_item.getState()) != command:
             rules_item.postUpdate(command)
-        if str(device.state) != str(command):
+        if str(device.getState()) != command:
             device.sendCommand(command)
+
+        self.logger.info("Quelle=" + source + " -> " + light_name + " = " + command)
