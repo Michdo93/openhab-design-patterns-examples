1640d57b7720660d8d88eebd68262c1104b92ad4
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-20 12:14:57 +0200

    Implement warning for device UNDEF state
    Log a warning when a device reports UNDEF state.

--- patterns/generic-is-alive/rules-python/generic_is_alive.py
+++ patterns/generic-is-alive/rules-python/generic_is_alive.py
@@ -5,4 +5,7 @@ from openhab.triggers import GroupStateChangeTrigger
 @rule(triggers=[GroupStateChangeTrigger("DeviceStatuses", state="UNDEF")])
 class ASensorStoppedReporting:
     def execute(self, module, input):
-        pass  # Meldung oder Alarm ausloesen
+        event = input.get("event")
+        item_name = event.getItemName() if event else "unbekannt"
+        self.logger.warn(item_name + " meldet sich nicht mehr (UNDEF) - Alarm/Meldung ausloesen")
+        # Meldung oder Alarm ausloesen
