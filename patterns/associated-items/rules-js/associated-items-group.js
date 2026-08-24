b6d4836aafee0dd771bf288145f4272ca3b731d1
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-24 20:17:23 +0200

    Change trigger and enhance item status search logic
    Updated the trigger from ItemStateChangeTrigger to GroupStateChangeTrigger for the associated items rule. Added multiple criteria for finding item statuses.

--- patterns/associated-items/rules-js/associated-items-group.js
+++ patterns/associated-items/rules-js/associated-items-group.js
@@ -1,10 +1,24 @@
 rules.JSRule({
   name: "Zugehoeriges Item ueber Gruppenzugehoerigkeit finden",
-  triggers: [triggers.ItemStateChangeTrigger("gSensors")],
+  triggers: [triggers.GroupStateChangeTrigger("gSensors")],
   execute: (event) => {
     const sensors = items.getItem("gSensors").members;
+
+    // Ansatz: Item-Name
     const status1 = sensors.find((i) => i.name === event.itemName + "_Status");
+
+    // Ansatz: Item-Tag
     const status2 = sensors.find((i) => i.tags.includes("Status"));
+
+    // Ansatz: Item-Tags
+    const status3 = sensors.find(
+      (i) => ["Status", "Power"].every((tag) => i.tags.includes(tag))
+    );
+
+    // Ansatz: mehrere Kriterien
+    const status4 = sensors.find(
+      (i) => i.tags.includes("Status") && i.name.endsWith("_Status") && i.type === "Switch"
+    );
 
     console.log("status1=" + (status1 && status1.name) + " status2=" + (status2 && status2.name));
   }
