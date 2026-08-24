fe635236a4e9ae0d68619bc6df905aaddaf9d2b2
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-24 19:05:33 +0200

    Update trigger type and clean up code

--- patterns/associated-items/rules-js/associated-items-group.js
+++ patterns/associated-items/rules-js/associated-items-group.js
@@ -1,25 +1,14 @@
+const { rules, triggers, items } = require('openhab');
+
 rules.JSRule({
   name: "Zugehoeriges Item ueber Gruppenzugehoerigkeit finden",
-  triggers: [triggers.GroupStateChangeTrigger("gSensors")],
+  triggers: [triggers.ItemGroupStateChangeTrigger("gSensors")], // Angepasst
   execute: (event) => {
-    const sensors = items.getItem("gSensors").members;
+    const sensors = items.getItem("gSensors").members; //[cite: 3]
 
-    // Ansatz: Item-Name
-    const status1 = sensors.find((i) => i.name === event.itemName + "_Status");
+    const status1 = sensors.find((i) => i.name === event.itemName + "_Status"); //[cite: 3]
+    const status2 = sensors.find((i) => i.tags.includes("Status")); //[cite: 3]
 
-    // Ansatz: Item-Tag
-    const status2 = sensors.find((i) => i.tags.includes("Status"));
-
-    // Ansatz: Item-Tags
-    const status3 = sensors.find(
-      (i) => ["Status", "Power"].every((tag) => i.tags.includes(tag))
-    );
-
-    // Ansatz: mehrere Kriterien
-    const status4 = sensors.find(
-      (i) => i.tags.includes("Status") && i.name.endsWith("_Status") && i.type === "Switch"
-    );
-
-    console.log("status1=" + (status1 && status1.name) + " status2=" + (status2 && status2.name));
+    console.log("status1=" + (status1 && status1.name) + " status2=" + (status2 && status2.name)); //[cite: 3]
   }
 });
