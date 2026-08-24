45b73ad1f72b1d733f844d5b333087ad3a218d62
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-24 19:05:06 +0200

    Update trigger method for associated items rule

--- patterns/associated-items/rules-js/associated-items-naming.js
+++ patterns/associated-items/rules-js/associated-items-naming.js
@@ -1,11 +1,13 @@
+const { rules, triggers, items } = require('openhab');
+
 rules.JSRule({
   name: "Zugehoeriges Item ueber Namenskonvention finden",
-  triggers: [triggers.GroupStateChangeTrigger("gSensors")],
+  triggers: [triggers.ItemGroupStateChangeTrigger("gSensors")], // Angepasst
   execute: (event) => {
-    if (event.itemName.endsWith("_Status")) {
+    if (event.itemName.endsWith("_Status")) { //[cite: 2]
       return;
     }
-    const statusItem = items.getItem(event.itemName + "_Status");
-    statusItem.postUpdate("ON");
+    const statusItem = items.getItem(event.itemName + "_Status"); //[cite: 2]
+    statusItem.postUpdate("ON"); //[cite: 2]
   }
 });
