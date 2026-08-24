ce57e44d7eb7fdb4d0574cb293e7128cc4727d68
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-24 19:52:10 +0200

    Change trigger type and clean up code

--- patterns/associated-items/rules-js/associated-items-group.js
+++ patterns/associated-items/rules-js/associated-items-group.js
@@ -1,14 +1,13 @@
-const { rules, triggers, items } = require('openhab');
+const { rules, triggers } = require('openhab');
 
 rules.JSRule({
   name: "Zugehoeriges Item ueber Gruppenzugehoerigkeit finden",
-  triggers: [triggers.ItemGroupStateChangeTrigger("gSensors")], // Angepasst
+  triggers: [triggers.ItemStateChangeTrigger("gSensors")],
   execute: (event) => {
-    const sensors = items.getItem("gSensors").members; //[cite: 3]
+    const sensors = items.getItem("gSensors").members;
+    const status1 = sensors.find((i) => i.name === event.itemName + "_Status");
+    const status2 = sensors.find((i) => i.tags.includes("Status"));
 
-    const status1 = sensors.find((i) => i.name === event.itemName + "_Status"); //[cite: 3]
-    const status2 = sensors.find((i) => i.tags.includes("Status")); //[cite: 3]
-
-    console.log("status1=" + (status1 && status1.name) + " status2=" + (status2 && status2.name)); //[cite: 3]
+    console.log("status1=" + (status1 && status1.name) + " status2=" + (status2 && status2.name));
   }
 });
