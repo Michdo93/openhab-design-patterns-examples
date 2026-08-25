e50fe1f3e8149a46e1d1cba0855146a620cabfff
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-25 13:54:04 +0200

    Add logging for Light1_Dimmer brightness changes

--- patterns/lichtsteuerungssystem/rules-js/lichtsteuerungssystem-dynamisch.js
+++ patterns/lichtsteuerungssystem/rules-js/lichtsteuerungssystem-dynamisch.js
@@ -5,8 +5,10 @@ rules.JSRule({
     const hour = time.ZonedDateTime.now().hour();
     if (hour >= 18 && hour < 22) {
       items.getItem("Light1_Dimmer").sendCommand(80); // Helligkeit auf 80%
+      console.log("Stunde=" + hour + " -> Light1_Dimmer 80%");
     } else {
       items.getItem("Light1_Dimmer").sendCommand(20); // Helligkeit auf 20%
+      console.log("Stunde=" + hour + " -> Light1_Dimmer 20%");
     }
   }
 });
