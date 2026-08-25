e7c931bb11d4a2e748313da43e292072c22ab521
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-25 13:54:40 +0200

    Add logging for light control actions

--- patterns/lichtsteuerungssystem/rules-js/lichtsteuerungssystem-zeit.js
+++ patterns/lichtsteuerungssystem/rules-js/lichtsteuerungssystem-zeit.js
@@ -4,5 +4,6 @@ rules.JSRule({
   execute: (event) => {
     items.getItem("PresetDimLevel").postUpdate(20);
     items.getItem("UpdateLightLevels").sendCommand("ON");
+    console.log("PresetDimLevel -> 20, UpdateLightLevels -> ON");
   }
 });
