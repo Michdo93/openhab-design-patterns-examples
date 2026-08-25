8d1dbd15c4a7dc64b68bfd542dc02fe170836f9e
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-08-25 13:54:23 +0200

    Add logging for motion detection in light control

--- patterns/lichtsteuerungssystem/rules-js/lichtsteuerungssystem-bewegung.js
+++ patterns/lichtsteuerungssystem/rules-js/lichtsteuerungssystem-bewegung.js
@@ -4,9 +4,12 @@ rules.JSRule({
   execute: (event) => {
     if (items.getItem("Light1").state === "OFF") {
       items.getItem("Light1").sendCommand("ON");
+      console.log("Bewegung erkannt - Light1 eingeschaltet (10 Min. Timer)");
       actions.ScriptExecution.createTimer(time.ZonedDateTime.now().plusMinutes(10), () => {
         items.getItem("Light1").sendCommand("OFF");
       });
+    } else {
+      console.log("Bewegung erkannt, Light1 war aber bereits an");
     }
   }
 });
